13ba44819768fda9b6b23641568ff76ce2454ba7
abu_saad_linkon <linkon.iit.gmail.com>
2023-10-31 14:48:00 +0600

    added links

--- Linkon/Health Care/index.js
+++ Linkon/Health Care/index.js
@@ -3,3 +3,19 @@ function appointmentAlert(){
 }
 
 
+
+function myFunctionforReadBtn() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
